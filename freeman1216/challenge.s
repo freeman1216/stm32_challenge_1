af5ea84750422beff320f6f4558fa6c1946e68fd
fufixman <freeman121634@gmail.com>
2026-02-06 11:21:21 +0100

    freeman1216: fixed a version that doesnt write to reserved bits

--- freeman1216/challenge.s
+++ freeman1216/challenge.s
@@ -16,12 +16,14 @@
 _reset:
     /*The idea is to use instructions to encode an address*/
     ldr.n r0, addr
-    /*third bit in fc is set so we can just store the address as is to enable gpio*/
-    str.n r0, [r0,#0x34]
+    /*bit 30 and bit 6 is set in the adress  so we can just shift it by 4 to get a mask*/
+    /*added benefit is that we dont write to reserved bits*/
+    lsrs r3,r0,#4
+    str.n r3, [r0,#0x34]
     /*to get bit 26 set*/
-    lsls r0,#9
-    str.n r0, [sp]
 dst1:
+    /*first write just gets ignored because the gpio is not initialised yet*/
+    str.n r3, [sp]
     /*steal sign bit*/
     asrs r1,r7,#20
     str.n r1, [sp,#20]
